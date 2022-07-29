7ed8f1809fc6d05392c21005ae8329cc8ca43996
ShobhitWK <shobhit@webkorps.com>
2022-07-29 20:23:46 +0530

    added db schema

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root 'pages#home'
-  get "/about", to: "pages#about"
+  get "about", to: "pages#about"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
