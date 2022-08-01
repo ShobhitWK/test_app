37b8829947bbde064e35caaa66619ec6c0284cfc
ShobhitWK <shobhit@webkorps.com>
2022-08-01 21:36:17 +0530

    enabled new articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,10 +2,8 @@ class ArticlesController < ApplicationController
   def show
 
     @article = Article.find(params[:id])
-
     rescue ActiveRecord::RecordNotFound => e
       render :error
-
     end
 
   def index
@@ -17,6 +15,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
-    render plain: @article.inspect
+    @article.save
+    redirect_to @article
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
   root 'pages#home'
-  get "about", to: "pages#about"
   resources :articles, only: [:show, :index, :new, :create]
 
 end
