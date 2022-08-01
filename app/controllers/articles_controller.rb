0436ce518cff792c62eb2233373d4cbda932683d
ShobhitWK <shobhit@webkorps.com>
2022-08-01 21:05:08 +0530

    new article add feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,18 @@ class ArticlesController < ApplicationController
 
     rescue ActiveRecord::RecordNotFound => e
       render :error
-      
+
     end
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    render plain: @article.inspect
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
 
   root 'pages#home'
   get "about", to: "pages#about"
-  get "articles", to: "articles#index"
-  resources :articles, only: [:show]
+  resources :articles, only: [:show, :index, :new, :create]
 
 end
