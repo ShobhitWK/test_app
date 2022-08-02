b7495f85184256613cd46c71ddbf9e46071c6cce
ShobhitWK <shobhit@webkorps.com>
2022-08-02 23:25:25 +0530

    added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,11 @@ class User < ApplicationRecord
   validates :username, presence: true,
                         length: {minimum: 3, maxmimum: 25},
                         uniqueness: {case_sensitive: false}
+
+  VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+
   validates :email, presence: true,
                         length: {minimum: 5, maxmimum: 105},
-                        uniqueness: {case_sensitive: false}
+                        uniqueness: {case_sensitive: false},
+                        format: { with: VALID_EMAIL_REGEX }
 end
